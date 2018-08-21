7e6b0330dfa215bd734ca125fe89ceee4a9aee84
pallavimaruji <36764894+pallavimaruji@users.noreply.github.com>
2018-08-21 18:07:08 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    char ch;
+    for(i=1;i<255;i++)
+    {
+        ch=i;
+        cout<<i<< "->"<<ch<<"\t";
+    }
+    return 0;
+}
